6efd310eb2aa90e06c0b573695353f7382a16a66
edyfox <edyfox@2a77ed30-b011-0410-a7ad-c7884a0aa172>
2007-03-01 07:52:11 +0000

    Sync with the CVS repository.
    git-svn-id: https://vim.svn.sourceforge.net/svnroot/vim/vim7@223 2a77ed30-b011-0410-a7ad-c7884a0aa172

--- src/pty.c
+++ src/pty.c
@@ -37,7 +37,7 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
-/* RCS_ID("$Id: pty.c,v 1.2 2006/11/01 17:12:46 vimboss Exp $ FAU") */
+/* RCS_ID("$Id: pty.c,v 1.3 2007/02/27 15:48:28 vimboss Exp $ FAU") */
 
 #include "vim.h"
 
