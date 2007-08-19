4a83b853f938d46f353f539ae4c2074621dbc404
edyfox <edyfox@2a77ed30-b011-0410-a7ad-c7884a0aa172>
2007-08-19 12:48:01 +0000

    Merged from the latest developing branch.
    git-svn-id: https://vim.svn.sourceforge.net/svnroot/vim/trunk@464 2a77ed30-b011-0410-a7ad-c7884a0aa172

---
+++ merge.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+svn merge \
+    https://vim.svn.sourceforge.net/svnroot/vim/trunk/ \
+    https://vim.svn.sourceforge.net/svnroot/vim/branches/vim7.1 \
+    .
--- src/version.c
+++ src/version.c
@@ -667,6 +667,12 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    82,
+/**/
+    81,
+/**/
+    80,
+/**/
     79,
 /**/
     78,
