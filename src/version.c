890193e30ae24bdaa6f36c7be8d3e76be41e1539
edyfox <edyfox@2a77ed30-b011-0410-a7ad-c7884a0aa172>
2008-07-20 04:56:51 +0000

    Merged from the latest developing branch.
    git-svn-id: http://vim.svn.sourceforge.net/svnroot/vim/trunk@1119 2a77ed30-b011-0410-a7ad-c7884a0aa172

--- src/version.c
+++ src/version.c
@@ -677,6 +677,14 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    9,
+/**/
+    8,
+/**/
+    7,
+/**/
+    6,
+/**/
     5,
 /**/
     4,
