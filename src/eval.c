20d9c19c0f887ed712e693c94014105c329b7474
kana <nicht@s8.xrea.com>
2008-09-12 00:32:05 +0900

    Fix has() - add prepareation +gauche/dyn

--- src/eval.c
+++ src/eval.c
@@ -11872,6 +11872,10 @@ f_has(argvars, rettv)
 	else if (STRICMP(name, "iconv") == 0)
 	    n = iconv_enabled(FALSE);
 #endif
+#ifdef DYNAMIC_GAUCHE
+	else if (STRICMP(name, "gauche") == 0)
+	    n = gauche_enabled(FALSE);
+#endif
 #ifdef DYNAMIC_MZSCHEME
 	else if (STRICMP(name, "mzscheme") == 0)
 	    n = mzscheme_enabled(FALSE);
